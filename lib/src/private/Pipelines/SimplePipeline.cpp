550c073d13f76f61ecb919bb4d4d17e98be9e0d2
Kevin Havranek <havranekkevin@gmail.com>
2018-01-06 23:05:19 +0100

    Add the path for shaders

--- lib/src/private/Pipelines/SimplePipeline.cpp
+++ lib/src/private/Pipelines/SimplePipeline.cpp
@@ -1,11 +1,28 @@
 #include <private/Pipelines/SimplePipeline.h>
 
+#include <boost/filesystem.hpp>
+
+namespace
+{
+    std::string GetVertexShaderPath()
+    {
+        const boost::filesystem::path path = boost::filesystem::current_path() / "../shaders/src/Simple/vert.spv";
+        return path.string();
+    }
+
+    std::string GetFragmentShaderPath()
+    {
+        const boost::filesystem::path path = boost::filesystem::current_path() / "../shaders/src/Simple/frag.spv";
+        return path.string();
+    }
+}
+
 namespace VT
 {
 SimplePipeline::SimplePipeline(const Swapchain& swapchain)
     : m_swapchain(swapchain)
-    , m_vertexShader(m_swapchain.GetRelatedDevice(), "")
-    , m_fragmentShader(m_swapchain.GetRelatedDevice(), "")
+    , m_vertexShader(m_swapchain.GetRelatedDevice(), GetVertexShaderPath())
+    , m_fragmentShader(m_swapchain.GetRelatedDevice(), GetFragmentShaderPath())
 {
 }
 
